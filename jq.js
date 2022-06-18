2a9c4367e405571358c9951795e79c8f628af9a6
P-Matejovic <107559716+P-Matejovic@users.noreply.github.com>
2022-06-18 16:13:54 +0200

    final
    But i dont know why i have space between dots, when i addet names.

--- jq.js
+++ jq.js
@@ -1,8 +1,41 @@
+
+
 $("document").ready(function(){
+console.log("Jq is working")
 
+//excercise1    
 
+const Names = ["Petr","Pavel","Michal","Oldřich","Pepa"];
 
-
-
+Names.forEach(function(item){
     
+    if (item === "Pavel") {
+        $(".ul").append("<li>"+item+"<li>");
+        $("Pavel").css("font-weight","bold")
+        
+    }
+    else {
+        $(".ul").append("<li>"+item+"<li>");
+        $("li").css("font-weight","normal")
+    };
+    
+   
+});
+
+//Excercise 2
+
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+};
+
+
+$("body").append("<H1>"+additionalBlock.title+"<H1>");
+$("body").append("<p>"+additionalBlock.text+"<p>").css("display", "block");
+
+
+
+
+
+
 });
